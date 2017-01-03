bdf3d8b56c8a39f98e40045cecddec90645b13d0
Unknown <davide.zaccheo@gmail.com>
2017-01-03 19:03:22 +0100

    release 1

---
+++ index.js
@@ -0,0 +1,153 @@
+var sync = require('synchronize');
+
+var synchObj = function (that, returnParamsNameArr) {
+    this.that = that;
+    this.returnParamsNameArr = returnParamsNameArr;
+}
+
+synchObj.prototype.executeFiberInt = function (fn, params) {
+    //var defer = sync.defer();
+    //runFiber(fn, params, defer)
+    if (this.returnParamsNameArr)
+        params.push(sync.defers.apply(sync, this.returnParamsNameArr))
+    else
+        params.push(sync.defer())
+    var res = sync.await(fn.apply(this.that, params));
+    return res;
+}
+synchObj.prototype.executeFiber = function (fn) {
+    //console.log(fn)
+    //console.log(arguments)
+    var params = [];
+    var argl = arguments.length
+    for (var i = 1; i < argl; i++) {
+        params.push(arguments[i]);
+    }
+    return this.executeFiberInt(fn, params);
+}
+
+var synchPromObj = function (that) {
+    this.that = that;
+
+
+}
+
+synchPromObj.prototype.runPromiseFiber = function (fn, params, defer) {
+    fn.apply(this.that, params).then(function (res) {
+        //fn().then(function (res) {
+        defer(null, res)
+    }).catch(function (e) {
+
+        defer(e)
+    })
+}
+
+synchPromObj.prototype.executePromiseFiberInt = function (fn, params) {
+    var defer = sync.defer();
+    this.runPromiseFiber(fn, params, defer)
+    var res = sync.await();
+    return res;
+}
+
+synchPromObj.prototype.executePromiseFiber = function (fn) {
+    //console.log(fn)
+    // console.log(arguments)
+    var params = [];
+    var argl = arguments.length
+    for (var i = 1; i < argl; i++) {
+        params.push(arguments[i]);
+    }
+    return this.executePromiseFiberInt(fn, params);
+}
+
+var runPromiseFiberFn = function (obj, fn, params, defer) {
+    fn.apply(obj, params).then(function (res) {
+        //fn().then(function (res) {
+        defer(null, res)
+    }).catch(function (e) {
+
+        defer(e)
+    })
+}
+
+exports.executePromiseFiberFnName = function (obj, fnName) {
+    //console.log(fn)
+    // console.log(arguments)
+    var params = [];
+    var argl = arguments.length
+    for (var i = 2; i < argl; i++) {
+        params.push(arguments[i]);
+    }
+    var fn = obj[fnName]
+
+    return executePromiseFiberIntFn(obj, fn, params);
+}
+
+exports.executePromiseFiberFn = function (obj, fn) {
+    //console.log(fn)
+    // console.log(arguments)
+    var params = [];
+    var argl = arguments.length
+    for (var i = 2; i < argl; i++) {
+        params.push(arguments[i]);
+    }
+    return executePromiseFiberIntFn(obj, fn, params);
+}
+
+var executePromiseFiberIntFn = function (obj, fn, params) {
+    var defer = sync.defer();
+    runPromiseFiberFn(obj, fn, params, defer)
+    var res = sync.await();
+    return res;
+}
+
+var executeFiberIntFn = function (obj, fn, params, returnParamsNameArr) {
+    //var defer = sync.defer();
+    //runFiber(fn, params, defer)
+
+    if (returnParamsNameArr)
+        params.push(sync.defers.apply(sync, returnParamsNameArr))
+    else
+        params.push(sync.defer())
+
+    console.log(params)
+    var res = sync.await(fn.apply(obj, params));
+    return res;
+}
+
+exports.executeFiberFn = function (obj, fn) {
+    //console.log(fn)
+    //console.log(arguments)
+    var params = [];
+    var argl = arguments.length
+    for (var i = 2; i < argl; i++) {
+        params.push(arguments[i]);
+    }
+    return executeFiberIntFn(obj, fn, params);
+}
+
+exports.executeFiberFnMultiParamCb = function (obj, fn, returnParamsNameArr) {
+    //console.log(fn)
+    //console.log(arguments)
+    var params = [];
+    var argl = arguments.length
+    for (var i = 3; i < argl; i++) {
+        params.push(arguments[i]);
+    }
+    return executeFiberIntFn(obj, fn, params, returnParamsNameArr);
+}
+
+
+
+
+exports.getSynchPromiseObj = function (that) {
+    return new synchPromObj(that)
+};
+
+exports.getSynchCallbackObj = function (that, returnParamsNameArr) {
+    return new synchObj(that, returnParamsNameArr)
+}
+
+exports.executeSynch = function (fn, done) {
+    sync.fiber(fn, done);
+}
---
+++ test.js
@@ -0,0 +1,128 @@
+//TEST
+
+var MongoClient = require('mongodb').MongoClient
+var assert = require("assert")
+
+
+var synchProm = require('./synchProm')
+
+
+var url = 'mongodb://localhost:27017/local';
+console.log("CALLBACK")
+MongoClient.connect(url, function (err, db) {
+    assert.equal(null, err);
+    db.close();
+});
+console.log("PROMISE")
+MongoClient.connect(url).then(function (res) {
+    var collection = res.collection('test_correctly_access_collections2');
+    assert.notEqual(null, collection);
+
+}).catch(function (err) {
+    // console.log("err:" + err);
+})
+
+console.log("SYNCH")
+
+
+var multiParamCB = function (a, b, c, multiCB) {
+    if (c)
+        b = c + b
+    if (c)
+        a = c + a
+
+
+    setTimeout(function () {
+        multiCB(null, a, b);
+    }, 3000);
+    // multiCB(null, a, b);
+
+
+
+
+}
+
+
+
+var testSynch = function () {
+    // var prom = synchProm.getSynchPromiseObj(MongoClient);
+    // var db = prom.executePromiseFiber(MongoClient.connect, url)
+    var db = synchProm.executePromiseFiberFn(MongoClient, MongoClient.connect, url)
+    assert.notEqual(null, db);
+    //var collection = res.collection('test_correctly_access_collections2');
+    var dbProm = synchProm.getSynchPromiseObj(db);
+    var stats = dbProm.executePromiseFiber(db.stats)
+    assert.notEqual(null, stats);
+    //console.log("stats:", stats)
+    var collection_test = db.collection("collection_test") //synchProm.executePromiseFiberFn(db, db.collection, "collection_test")
+    var cursor = collection_test.find({});
+
+    var resProm = synchProm.executePromiseFiberFn(cursor, cursor.toArray)
+    assert.notEqual(null, resProm);
+
+    cursor = collection_test.find({});
+    var resCb = synchProm.executeFiberFn(cursor, cursor.toArray)
+    assert.notEqual(null, resCb);
+
+
+    var resMultiCb = synchProm.executeFiberFnMultiParamCb(this, multiParamCB, ['res1', 'res2'], "a", "b", "c")
+    console.log("res resMultiCb:", resMultiCb)
+    assert.notEqual(null, resMultiCb);
+
+
+    cursor = collection_test.find({});
+    var cursorProm = synchProm.getSynchPromiseObj(cursor);
+    var res = cursorProm.executePromiseFiber(cursor.toArray)
+    console.log("res OBJ:", res)
+    return res
+}
+
+
+var testSynchName = function () {
+    // var prom = synchProm.getSynchPromiseObj(MongoClient);
+    // var db = prom.executePromiseFiber(MongoClient.connect, url)
+    var db = synchProm.executePromiseFiberFnName(MongoClient, 'connect', url)
+    assert.notEqual(null, db);
+    //var collection = res.collection('test_correctly_access_collections2');
+    var dbProm = synchProm.getSynchPromiseObj(db);
+    var stats = dbProm.executePromiseFiber(db.stats)
+    assert.notEqual(null, stats);
+    //console.log("stats:", stats)
+    var collection_test = db.collection("collection_test") //synchProm.executePromiseFiberFn(db, db.collection, "collection_test")
+    var cursor = collection_test.find({});
+
+    var resProm = synchProm.executePromiseFiberFnName(cursor, 'toArray')
+    assert.notEqual(null, resProm);
+
+    cursor = collection_test.find({});
+    var resCb = synchProm.executeFiberFn(cursor, cursor.toArray)
+    assert.notEqual(null, resCb);
+
+
+    var resMultiCb = synchProm.executeFiberFnMultiParamCb(this, multiParamCB, ['res1', 'res2'], "a", "b", null)
+    console.log("res resMultiCb:", resMultiCb)
+    assert.notEqual(null, resMultiCb);
+
+
+    cursor = collection_test.find({});
+    var cursorProm = synchProm.getSynchPromiseObj(cursor);
+    var res = cursorProm.executePromiseFiber(cursor.toArray)
+    console.log("res OBJ:", res)
+    return res
+}
+
+
+synchProm.executeSynch(testSynch, function (err, res) {
+    if (err)
+        console.log("err", err)
+    else
+        console.log("OK:", JSON.stringify(res, null, 4))
+})
+
+
+synchProm.executeSynch(testSynchName, function (err, res) {
+    if (err)
+        console.log("err", err)
+    else
+        console.log("OK:", JSON.stringify(res, null, 4))
+})
